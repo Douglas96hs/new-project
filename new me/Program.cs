8ec49d98c5ebae9eab69187fc5d69cb4fac447e0
unknown <dandisondouglas96@gmail.com>
2025-01-11 20:24:09 +0100

    initial commit

---
+++ new me/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+internal class Program
+{
+    public Program()
+    {
+    }
+
+    private static void Main(string[] args)
+    {
+        byte number = 2;
+        int count = 10;
+        float totalPrice = 20.95f;
+        char character = 'A';
+        string firstName = "Douglas";
+        bool isWorking = false;
+
+        Console.WriteLine(number);
+        Console.WriteLine(count);
+        Console.WriteLine(totalPrice);
+        Console.WriteLine(Character);
+        Console.WriteLine(firstName);
+        Console.WriteLine(isWorking);
+    }
+}
